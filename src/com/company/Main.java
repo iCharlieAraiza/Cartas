bb53438279049baca91e0e603e9b01e7ef0d3be9
Carlos Araiza <caraiza_10@hotmail.com>
2020-02-12 22:00:31 -0600

    actualizacion de los nombres

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         for(int i = 0; i < 4; i++){
             for (int j= 0; j< 13; j++){
-                b = ((j+2) == 11 ) ? ("Jack ") : ((j+2) == 12 ) ? "Reina " : ((j+2) == 13 ) ? "rey " : "as ";
+                b = ((j+2) == 11 ) ? ("Jack ") : ((j+2) == 12 ) ? "Reina " : ((j+2) == 13 ) ? "Rey " : "As ";
                 tipo = (i==0) ? "Espadas" : i==1 ?  "Corazones" : i==2 ? "Rombos" : "Treboles";
                 A[k] = ((j+2 < 11) ? String.valueOf(j+2) : b)  + " de " + tipo ;
                 k++;
