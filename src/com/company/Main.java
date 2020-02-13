40cf54f8028b4062ac6190a330c6289b33e22b23
Carlos Araiza <caraiza_10@hotmail.com>
2020-02-12 21:34:35 -0600

    Practica de la baraja

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String tipo, b;
+        String A[] = new String[52];
+        int k = 0;
+
+
+        for(int i = 0; i < 4; i++){
+            for (int j= 0; j< 13; j++){
+                b = ((j+2) == 11 ) ? "Jack " : ((j+2) == 12 ) ? "Reina " : ((j+2) == 13 ) ? "rey " : "as ";
+                tipo = (i==0) ? "Espadas" : i==1 ? b + "Corazones" : i==2 ? "Rombos" : "Treboles";
+                A[k] = ((j+2 < 11) ? String.valueOf(j+2) : b)  + " de " + tipo ;
+                k++;
+            }
+        }
+
+        for (int i=0; i<52; i++){
+            System.out.println(A[i]);
+        }
+    }
+}
